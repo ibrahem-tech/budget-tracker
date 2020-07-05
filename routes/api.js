a4ecc945937fce7e48dc4019c5b56e318539547e
Your Name <ryibrahem1210@gmail.com>
2020-07-05 13:45:53 -0400

    routes and odels

---
+++ routes/api.js
@@ -0,0 +1,37 @@
+const router = require("express").Router();
+const Transaction = require("../models/transaction.js");
+
+// Add new transaction, whether negative or positive
+router.post("/api/transaction", ({body}, res) => {
+  Transaction.create(body)
+    .then(dbTransaction => {
+      res.json(dbTransaction);
+    })
+    .catch(err => {
+      res.status(404).json(err);
+    });
+});
+
+// Maybe for SW?? 
+router.post("/api/transaction/bulk", ({body}, res) => {
+  Transaction.insertMany(body)
+    .then(dbTransaction => {
+      res.json(dbTransaction);
+    })
+    .catch(err => {
+      res.status(404).json(err);
+    });
+});
+
+// Grab all transactions
+router.get("/api/transaction", (req, res) => {
+  Transaction.find({}).sort({date: -1})
+    .then(dbTransaction => {
+      res.json(dbTransaction);
+    })
+    .catch(err => {
+      res.status(404).json(err);
+    });
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -3,33 +3,26 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-
-
-const PORT = process.env.PORT || 3006;
+const PORT = process.env.PORT || 7000;
 
 const app = express();
-
 
 app.use(logger("dev"));
 
 app.use(compression());
-
 app.use(express.urlencoded({ extended: true }));
-app .use(express.json());
+app.use(express.json());
 
+app.use(express.static("public"));
 
-//------Public -----//
-
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", { 
-    useNewUrlParser: true,
-    useFindAndModify: false,
-   
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+  useNewUrlParser: true,
+  useFindAndModify: false
 });
 
-//----routes----//
-
-
+// routes
+app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
-    console.log(`App running on port ${PORT}!`);
-  });
+  console.log(`App running on port ${PORT}!`);
+});
